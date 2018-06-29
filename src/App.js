3e7f3f2f32b5fa320b3a2b1626a84cac5514b91f
Hannah Thompson <hthompson@scentregroup.com>
2018-06-30 09:25:12 +1000

    feat: add webcam package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Webcam from "react-webcam";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -10,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <Webcam />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
