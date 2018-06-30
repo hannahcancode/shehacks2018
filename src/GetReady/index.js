76a24d6c2d828c76078324763141a1446f3bb02f
Hannah Thompson <hthompson@scentregroup.com>
2018-06-30 14:30:21 +1000

    feat(webcam): you look fantastic!

--- src/GetReady/index.js
+++ src/GetReady/index.js
@@ -1,20 +1,15 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
+import style from "./index.css";
 
 export const GetReady = () => (
-  <Fragment>
-    <div>
-      <p>Get Ready!</p>
+  <div className="getReady">
+    <p>First, we need a photo!</p>
+    <div className="button">
+      <Button variant="raised" color="primary" component={Link} to="/photo">
+        Take my photo!
+      </Button>
     </div>
-    <Button
-      variant="raised"
-      className="form"
-      color="primary"
-      component={Link}
-      to="/photo"
-    >
-      Take my photo!
-    </Button>
-  </Fragment>
+  </div>
 );
--- src/Photo/index.js
+++ src/Photo/index.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
 import Webcam from "react-webcam";
+import YouLookGreat from "../assets/youlookfantastic.png";
+import style from "./index.css";
 
 export class Photo extends Component {
+  constructor() {
+    super();
+    this.state = { currentCount: 0 };
+  }
+
+  componentDidMount() {
+    const intervalId = setInterval(this.runTimer, 2000);
+  }
+
+  runTimer = () => {
+    if (this.state.currentCount < 6) {
+      this.setState({ currentCount: this.state.currentCount + 1 });
+    } else {
+      clearInterval(this.intervalId);
+    }
+  };
+
   render() {
     return (
-      <Webcam
-        width={375}
-        height={667}
-        videoContraints={{ height: 667, width: 375 }}
-      />
+      console.log(this.state.currentCount) ||
+      (this.state.currentCount < 5 ? (
+        <Webcam
+          className="video"
+          width={375}
+          height={667}
+          videoContraints={{ height: 667, width: 375 }}
+        />
+      ) : (
+        <div className="background" />
+      ))
     );
   }
 }
